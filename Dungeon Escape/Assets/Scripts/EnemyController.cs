fff796fb3cd518ee4d05bca67ac03c51d7da8625
Kirigoulou <haouasyazid75@gmail.com>
2022-01-21 01:06:01 +0100

    cursed commit nothing works might revert (added ia that should follow the player btw)

---
+++ Dungeon Escape/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 10f;
+
+    private Transform target;
+    private NavMeshAgent agent;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float distance = Vector3.Distance(target.position, transform.position);
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
